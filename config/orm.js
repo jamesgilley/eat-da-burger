8253c4b053623cac249e3caeda23276424d538ee
James Gilley <jamesgilley1@icloud.com>
2021-01-13 13:09:55 -0600

    finishing code for orm

--- config/orm.js
+++ config/orm.js
@@ -13,3 +13,21 @@ const selectAll = () => {
     return query('select * from burgers')
         .catch(err => console.error('Error selectAll', err))
 };
+
+const insertOne = (burger) => {
+    const { burger_name, devoured } = burger
+    return query('insert into burgers(burger_name, devoured) values(?, ?)', [burger_name, devoured])
+        .catch(err => console.error('Error insertOne', err))
+};
+
+const updateOne = (burger) => {
+    const { ID, burger_name, devoured } = burger
+    return query('update burgers set burger_name = ?, devoured = ? where ID = ?', [burger_name, devoured, ID])
+        .catch(err => console.error('Error updateOne', err))
+}
+
+module.exports = {
+    selectAll,
+    insertOne,
+    updateOne
+}
